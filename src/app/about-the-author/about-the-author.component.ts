22d11e054ce8dac4e5b46cdcc7453f351cf0aae6
James <james.distef@gmail.com>
2025-03-26 12:56:47 -0400

    update to angular 19, hide top nav on mobile

--- src/app/about-the-author/about-the-author.component.ts
+++ src/app/about-the-author/about-the-author.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-about-the-author',
-    imports: [],
-    templateUrl: './about-the-author.component.html'
+  selector: 'app-about-the-author',
+  templateUrl: './about-the-author.component.html',
 })
 export class AboutTheAuthorComponent {}
--- src/app/about-this-page/about-this-page.component.ts
+++ src/app/about-this-page/about-this-page.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-about-this-page',
-    imports: [],
-    templateUrl: './about-this-page.component.html'
+  selector: 'app-about-this-page',
+  templateUrl: './about-this-page.component.html',
 })
 export class AboutThisPageComponent {}
