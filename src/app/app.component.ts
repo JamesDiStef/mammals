afa70d5c10f308895194eb36968d0b2134025b3e
James <james.distef@gmail.com>
2025-03-31 14:17:20 -0400

    add mobile drop down menu for small screen, and font awesome icon for same

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'animals';
+  mobileNavOpen = false;
+
+  toggleMenu() {
+    console.log('yo yo');
+    this.mobileNavOpen = !this.mobileNavOpen;
+  }
 }
