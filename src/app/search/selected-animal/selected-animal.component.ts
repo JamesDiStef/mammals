f7ed633c825037cefb30bda6907b0b8d4b1a2413
James <james.distef@gmail.com>
2025-03-31 17:48:22 -0400

    styling fixes, add edit icon

--- src/app/search/selected-animal/selected-animal.component.ts
+++ src/app/search/selected-animal/selected-animal.component.ts
@@ -1,12 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Animal } from '../../services/search.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'selected-animal',
-    imports: [],
-    templateUrl: './selected-animal.component.html'
+  selector: 'selected-animal',
+  imports: [FontAwesomeModule],
+  templateUrl: './selected-animal.component.html',
 })
 export class SelectedAnimalComponent {
+  faEdit = faEdit;
+  toggleMenu() {
+    console.log('editing');
+  }
   @Input() animal!: Animal | undefined;
 
   constructor() {}
